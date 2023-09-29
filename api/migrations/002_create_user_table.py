a285f6c1380399c58cf1993237def7b33d2a5070
Paola Alcala <paolavalcala@gmail.com>
2023-09-28 20:42:25 -0700

    removed password

--- api/migrations/002_create_user_table.py
+++ api/migrations/002_create_user_table.py
@@ -9,7 +9,6 @@ steps = [
             last_name VARCHAR(100) NOT NULL,
             email VARCHAR(100) NOT NULL,
             hashed_password VARCHAR(100) NOT NULL,
-            password VARCHAR(100) NOT NULL
         );
         """,
         # "Down" SQL statement
