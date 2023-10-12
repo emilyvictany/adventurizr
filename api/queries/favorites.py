213e54ad76938c212c9381569930314a9c0d6578
Erin Gerber <erin.gerber3@gmail.com>
2023-10-12 19:04:18 +0000

    Resolve "endpoint - POST Add Activity to User Favorites"

--- api/queries/favorites.py
+++ api/queries/favorites.py
@@ -1,5 +1,33 @@
 import os
 from pydantic import BaseModel
 from typing import List, Union
+from fastapi import HTTPException
+from queries.pool import pool
+from queries.users import UserQueries
 
 
+
+
+
+
+
+
+class FavoriteRepository:
+
+    def create_favorite(self, user_id:int, activity_id:int)-> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        INSERT INTO favorites (user_id, activity_id)
+                        VALUES (%s, %s)
+                        RETURNING user_id, activity_id
+                        """,
+                        [user_id, activity_id]
+                    )
+                    conn.commit()
+            return True
+
+        except Exception as e:
+            return False
--- api/routers/favorites.py
+++ api/routers/favorites.py
@@ -8,8 +8,25 @@ from fastapi import (
 )
 from jwtdown_fastapi.authentication import Token
 from authenticator import authenticator
+from queries.favorites import FavoriteRepository
+from queries.users import UserOut
+from queries.activities import ActivityOut
+
 
 from pydantic import BaseModel
 
 
 router = APIRouter()
+
+
+
+@router.post("/api/favorites/{user_id}/{activity_id}", response_model=bool)
+def create_favorite(
+    user_id:int,
+    activity_id:int,
+    repo: FavoriteRepository=Depends(),
+    account_data: dict=Depends(authenticator.get_current_account_data),
+):
+    user_id=account_data["id"]
+
+    return repo.create_favorite(user_id, activity_id)
