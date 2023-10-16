c36448d61e5269760839a56ec602753973117ccf
Emily <emilyvictany@gmail.com>
2023-10-16 23:46:00 +0000

    Revert "finished"

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,18 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Nav />
-        <div className="container">
-          <Routes>
+    <BrowserRouter>
+      <Nav />
+      <div className="container">
+        <Routes>
 
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </AuthProvider>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
